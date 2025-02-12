be0aff4cb10f896d79a347431d42883f36d73e81
Dexolotlterrus <dexolotlterrus@gmail.com>
2025-02-12 00:34:46 -0500

    replaced photos

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -91,7 +91,7 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Congratulations! You have completed the game.
+            Happy Valentine's Day Cutie!
           </motion.h2>
         )}
         {step === 1 && (
@@ -132,7 +132,7 @@ export default function ValentinesProposal() {
             <h2
               className={`text-5xl font-semibold mb-8 ${playfairDisplay.className}`}
             >
-              Will you be my Valentine?
+              I think we'd make a cute MATCHING PAIR at prom :)
             </h2>
             <Image
               src="/sad_hamster.png"
@@ -145,7 +145,7 @@ export default function ValentinesProposal() {
                 className="px-6 py-2 text-lg font-semibold text-white bg-gradient-to-r from-pink-500 to-rose-500 rounded-xl hover:from-pink-600 hover:to-rose-600 transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl"
                 onClick={handleYesClick}
               >
-                Yes, I will! 🥰
+                Yes, let's match at prom! 🥰
               </button>
               <button
                 className="px-6 py-2 text-lg font-semibold text-white bg-gradient-to-r from-gray-500 to-gray-600 rounded-xl hover:from-gray-600 hover:to-gray-700 transform hover:scale-95 transition-all duration-300 shadow-lg"
@@ -160,7 +160,7 @@ export default function ValentinesProposal() {
                 }
                 onMouseEnter={() => setPosition(getRandomPosition())}
               >
-                No, I won&apos;t 😢
+                I&apos;d rather jump into a tub full of cats 😢
               </button>
             </div>
           </motion.div>
@@ -174,8 +174,8 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Thank you for accepting, I love you! 💕
-            <p className="text-sm mt-4">For more information, write me!!! 💌</p>
+            I love you! 💕
+            <p className="text-sm mt-4">See you there!</p>
             <Image
               src="/hamster_jumping.gif"
               alt="Hamster Feliz"
